e806323793b68424d7a64a16787f191d129be39c
Felix Wieland <fwieland@ibs.local>
2024-05-13 16:06:48 +0200

    commit from office

--- database.js
+++ database.js
@@ -2,17 +2,27 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "testuser",
-  password: "testuser",
+  user: "root",
+  password: "",
   database: "mydb"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "INSERT INTO customers (name,address) VALUES ('max', 'Musterstraße 12');";
+  var sql = "CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'testuser';";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table altered");
+    console.log(result);
+  });
+  var sql = "GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'testuser'@'localhost' WITH GRANT OPTION;";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+  var sql = "SELECT User, Host, authentication_string FROM mysql.user";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
   });
 });
